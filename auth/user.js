aa8db47b981c2e8be62dcdb9088122ebdc48ee99
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-26 16:46:30 -0500

    complete part C?

--- auth/user.js
+++ auth/user.js
@@ -2,11 +2,19 @@ import { getUser, signOutUser } from '../fetch-utils.js';
 
 const signOutLink = document.getElementById('sign-out-link');
 
-// make sure we have a user!
 // > Part C:
+// make sure we have a user!
+const user = getUser();
 //     - get the user (if there is one)
 //     - check if there is not a user and if so do the redirect
-//       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+if (!user) {
+    const base =
+        location.pathname === '/' || location.pathname === '/solutions-web/user-auth/'
+            ? './'
+            : '../';
+    //       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+    location.replace(`${base}auth/?redirectUrl=${encodeURIComponent(location)}`);
+}
 
 // > Part B: attach event listener to signOutLink that calls signOutUser
 signOutLink.addEventListener('click', signOutUser);
